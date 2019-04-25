8f6f247577042baca0d6eaf00b14fbe48e11ad30
Cailongjun <cailongjun@huami.com>
2019-04-25 14:04:26 +0800

    Initial Commit

---
+++ CollectionKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionKitDemo
+//
+//  Created by Huacai on 2019/4/25.
+//  Copyright © 2019 Huacai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
